e3f35b57e22dbf04a08975cce66c329855f461f9
spx0315 <1445024781@qq.com>
2018-12-22 22:36:10 +0800

    更改

---
+++ app/src/main/java/com/example/keji0/login1application/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.keji0.login1application;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+        getWindow().setBackgroundDrawableResource(R.drawable.bg_main);
+
+    }
+
+
+}
--- app/src/main/java/com/example/keji0/login1application/MainActivity.java
+++ app/src/main/java/com/example/keji0/login1application/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.keji0.login1application;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends Activity {
+    private ImageButton btn_qq;
+    private ImageButton btn_wx;
+    private ImageButton btn_wy;
+    private TextView text3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
         getWindow().setBackgroundDrawableResource(R.drawable.bg_main);
+
+        btn_qq=(ImageButton)findViewById(R.id.btn_qq);
+        btn_wx=(ImageButton)findViewById(R.id.btn_wx);
+        btn_wy=(ImageButton)findViewById(R.id.btn_wy);
+        text3=(TextView)findViewById(R.id.text3);
+
+        btn_qq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+            }
+        });
+        btn_wx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+            }
+        });
+        btn_wy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+            }
+        });
+        text3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+            }
+        });
     }
 }
